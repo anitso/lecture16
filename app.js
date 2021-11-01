ef254a5e84d16b26660a6e6a077276505a5c61d1
annyts <90929385+annyts@users.noreply.github.com>
2021-11-01 18:14:32 +0400

    add js

---
+++ app.js
@@ -0,0 +1,53 @@
+//task1
+
+function factorial(n) {
+    if ((n = 1) !==0))
+      return true;
+    else
+      return false;
+  }
+
+  console.log(oddOrEven(3));
+  console.log(oddOrEven(6));
+
+  // Task 2:
+ 
+  function getCurrencySymbolFromCode
+
+  let code=[h];
+  let i;
+  for (i = 0; i != t.length; i++)
+  code[i] = h(t[i]);
+  return code;
+   }
+   let h = function(k) {
+     if (k='EUR')           
+   return  '€';
+   else if (k='USD')
+   return '$';
+   else if (k='GEL')
+   return '₾'
+   else return undefined;
+   }
+   let  codes=["GEL", "EUR", "USD", "FR", "YEN", "YUAN"];
+   let r = getCurrencySymbolFromCode(h,codes);
+   console.log(r);
+
+//task3
+
+function upperToLower(text) {
+    let textResult = text.toLowerCase();
+    console.log(textResult);
+upperToLower("MY NAME IS JOHN");
+upperToLower("I AM BULLETPROOF!");
+}
+
+//task4
+
+function filterarray(numarray) {
+    const result = numarray.filterarray
+    numarray=[2,4,6,];
+    console.log(result);
+}
+
+
